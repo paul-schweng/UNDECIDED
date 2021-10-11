24d0a84c4930b6c2532d1932b348594ec9d11d13
Paul Schweng <paul.schweng@siemens.com>
2021-10-11 21:06:22 +0200

    added UI + some functionalities

--- Backend/src/main/java/cyou/ted2/undecided/UndecidedApplication.java
+++ Backend/src/main/java/cyou/ted2/undecided/UndecidedApplication.java
@@ -2,12 +2,29 @@ package cyou.ted2.undecided;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@RestController
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class UndecidedApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UndecidedApplication.class, args);
+    }
+
+    @RequestMapping("/resource")
+    public Map<String,Object> home() {
+        Map<String,Object> model = new HashMap<>();
+        model.put("id", UUID.randomUUID().toString());
+        model.put("content", "Hello World");
+        return model;
     }
 
 }
