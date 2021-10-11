c96c6effa5a90bfa823538d91cbd6663c08d0bd5
Paul Schweng <paul.schweng@siemens.com>
2021-10-11 19:44:53 +0200

    init project

---
+++ Backend/src/test/java/com/undecided/undecided/UndecidedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.undecided.undecided;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UndecidedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
