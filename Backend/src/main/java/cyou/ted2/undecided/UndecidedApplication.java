1fb4ee579b470a6706e349eb8caca4463deb880e
Paul Schweng <paul.schweng@siemens.com>
2021-10-11 20:18:51 +0200

    added backend

---
+++ Backend/src/main/java/cyou/ted2/undecided/UndecidedApplication.java
@@ -0,0 +1,13 @@
+package cyou.ted2.undecided;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UndecidedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UndecidedApplication.class, args);
+    }
+
+}
---
+++ Backend/src/test/java/cyou/ted2/undecided/UndecidedApplicationTests.java
@@ -0,0 +1,13 @@
+package cyou.ted2.undecided;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UndecidedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
