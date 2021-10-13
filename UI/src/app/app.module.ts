e070d9e870845ab4fc97be72bb3be64bf0bd1125
Paul Schweng <paul.schweng@siemens.com>
2021-10-13 17:11:02 +0200

    added translation service + busy-div

--- UI/src/app/app.module.ts
+++ UI/src/app/app.module.ts
@@ -4,7 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './components/hello-world/hello-world.component';
-import {HttpClientModule} from "@angular/common/http";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {NgBusyModule} from "ng-busy";
 
 @NgModule({
   declarations: [
@@ -14,9 +18,22 @@ import {HttpClientModule} from "@angular/common/http";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
+    BrowserAnimationsModule,
+    NgBusyModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
--- UI/src/app/components/hello-world/hello-world.component.ts
+++ UI/src/app/components/hello-world/hello-world.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'hello-world',
@@ -10,11 +11,23 @@ export class HelloWorldComponent implements OnInit {
 
   title = 'Demo';
   greeting: any = {};
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient,
+              public translate: TranslateService) {
     http.get('/api/resource').subscribe(data => this.greeting = data);
   }
 
   ngOnInit(): void {
+    this.translate.addLangs(['de','en']);
+    this.translate.setDefaultLang('en');
+
+    if(this.translate.getLangs().includes(this.translate.getBrowserLang()))
+      this.translate.use(this.translate.getBrowserLang());
+    else
+      this.translate.use(this.translate.getDefaultLang());
+  }
+
+  switchLang(lang: string) {
+    this.translate.use(lang);
   }
 
 }
