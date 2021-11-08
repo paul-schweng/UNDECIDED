45cfde5853ca6b60d3548872294d957e9902a4ae
Paul Schweng <paul.schweng@siemens.com>
2021-11-09 00:23:00 +0100

    added basic authentification (no sessions stored on backend) [UU-43]

---
+++ Backend/src/test/java/cyou/ted2/undecided/StepDefinitions.java
@@ -0,0 +1,34 @@
+package cyou.ted2.undecided;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class StepDefinitions {
+    @Given("is logged in")
+    public void isLoggedIn() {
+    }
+
+    @Given("rate is clicked")
+    public void rateIsClicked() {
+    }
+
+    @Then("upload picture")
+    public void uploadPicture() {
+    }
+
+    @Then("enter product")
+    public void enterProduct() {
+    }
+
+    @Then("enter rating")
+    public void enterRating() {
+    }
+
+    @Then("post rating")
+    public void postRating() {
+    }
+
+    @Then("tag friends")
+    public void tagFriends() {
+    }
+}
