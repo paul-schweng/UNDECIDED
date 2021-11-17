70bc7f59841941691e09be9ff279eeb978199cfe
paul-schweng <paul.schweng@siemens.com>
2021-11-17 14:40:09 +0100

    added models to back-/frontend [UU-49]
    changes to rating model

---
+++ Backend/src/main/java/cyou/ted2/undecided/models/Vote.java
@@ -0,0 +1,9 @@
+package cyou.ted2.undecided.models;
+
+import java.time.LocalDateTime;
+
+public class Vote {
+    private User user;
+    private Rating rating;
+    private LocalDateTime timestamp;
+}
