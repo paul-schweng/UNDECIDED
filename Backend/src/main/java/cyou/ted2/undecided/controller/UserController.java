9f7c2ffe91e2493becbbea66d38632bbfa9b3af6
Elisa Schäfer <elisa@elmama.de>
2021-11-29 19:56:20 +0100

    created UserController & UserRepository

---
+++ Backend/src/main/java/cyou/ted2/undecided/controller/UserController.java
@@ -0,0 +1,50 @@
+package cyou.ted2.undecided.controller;
+
+import cyou.ted2.undecided.models.Rating;
+import cyou.ted2.undecided.models.User;
+import cyou.ted2.undecided.repository.RatingRepository;
+import cyou.ted2.undecided.repository.UserRepository;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/user")
+class UserController {
+
+    private final UserRepository repository;
+
+    UserController(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @PostMapping("/email")
+    @ResponseBody
+    User postNewEmail(@RequestBody String currentEmail, String newEmail) {
+        User user = repository.findUserByEmail(currentEmail);
+        user.setEmail(newEmail);
+        return repository.save(user);
+    }
+
+    /*@PutMapping("/password")
+    @ResponseBody
+    User putNewEmail(@RequestBody String currentPw, String newPw) {
+        User user = repository.findUserByPassword(currentPw); //TODO: userzuordnung?
+        user.setPassword(newPw);
+        return repository.save(user);
+    }*/
+
+    @GetMapping()
+    @ResponseBody
+    User getUser(){return null;}//TODO
+
+    @PutMapping()
+    @ResponseBody
+    User putUpdateUser(){return null;}//TODO
+
+
+
+
+
+
+
+
+}
--- Backend/src/main/java/cyou/ted2/undecided/models/User.java
+++ Backend/src/main/java/cyou/ted2/undecided/models/User.java
@@ -18,4 +18,132 @@ public class User {
     private LocalDate birthdate, registerDate;
     private boolean verified, isDarkTheme;
     private int ratingsNum, followerNum, followingNum;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsertype() {
+        return usertype;
+    }
+
+    public void setUsertype(String usertype) {
+        this.usertype = usertype;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getProfileImage() {
+        return profileImage;
+    }
+
+    public void setProfileImage(String profileImage) {
+        this.profileImage = profileImage;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public LocalDate getRegisterDate() {
+        return registerDate;
+    }
+
+    public void setRegisterDate(LocalDate registerDate) {
+        this.registerDate = registerDate;
+    }
+
+    public boolean isVerified() {
+        return verified;
+    }
+
+    public void setVerified(boolean verified) {
+        this.verified = verified;
+    }
+
+    public boolean isDarkTheme() {
+        return isDarkTheme;
+    }
+
+    public void setDarkTheme(boolean darkTheme) {
+        isDarkTheme = darkTheme;
+    }
+
+    public int getRatingsNum() {
+        return ratingsNum;
+    }
+
+    public void setRatingsNum(int ratingsNum) {
+        this.ratingsNum = ratingsNum;
+    }
+
+    public int getFollowerNum() {
+        return followerNum;
+    }
+
+    public void setFollowerNum(int followerNum) {
+        this.followerNum = followerNum;
+    }
+
+    public int getFollowingNum() {
+        return followingNum;
+    }
+
+    public void setFollowingNum(int followingNum) {
+        this.followingNum = followingNum;
+    }
 }
