bb1b86827c1ce04af24ab64e102b208c38ead4ac
2002chris <christian.geier02@t-online.de>
2021-12-02 17:23:35 +0100

    fixed redirect bug

--- Backend/src/main/java/cyou/ted2/undecided/Authentication.java
+++ Backend/src/main/java/cyou/ted2/undecided/Authentication.java
@@ -9,7 +9,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
+/*
 @Configuration
 @EnableWebSecurity
 public class Authentication extends WebSecurityConfigurerAdapter {
@@ -42,4 +42,4 @@ public class Authentication extends WebSecurityConfigurerAdapter {
         return new InMemoryUserDetailsManager(user);
     }
 }
-
+*/
