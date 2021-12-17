3ad950abfb896b5c89fe39abe38d6dceb682f3a1
Paul Schweng <paul.schweng@siemens.com>
2021-12-17 01:44:59 +0100

    added Authentication model

---
+++ Backend/src/main/java/cyou/ted2/undecided/models/Authentication.java
@@ -0,0 +1,52 @@
+package cyou.ted2.undecided.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.time.LocalDateTime;
+
+@Entity
+public class Authentication extends Model{
+
+    @Id
+    protected String session;
+
+    @ManyToOne
+    @JoinColumn(name = "userid")
+    protected User user;
+    protected LocalDateTime lastLogin;
+    protected String ip;
+
+    public String getSession() {
+        return session;
+    }
+
+    public void setSession(String session) {
+        this.session = session;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDateTime getLastLogin() {
+        return lastLogin;
+    }
+
+    public void setLastLogin(LocalDateTime lastLogin) {
+        this.lastLogin = lastLogin;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+}
