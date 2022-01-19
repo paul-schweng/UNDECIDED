12c41c52b58c7792e42b733d870fa74c23848ea6
Elisa Schäfer <elisa@elmama.de>
2022-01-19 19:55:59 +0100

    unfinished temporary imageController

--- Backend/src/main/java/cyou/ted2/undecided/controller/ImageController.java
+++ Backend/src/main/java/cyou/ted2/undecided/controller/ImageController.java
@@ -1,12 +1,11 @@
 package cyou.ted2.undecided.controller;
 
-import cyou.ted2.undecided.models.Rating;
-import cyou.ted2.undecided.models.User;
 import cyou.ted2.undecided.repository.RatingRepository;
 import cyou.ted2.undecided.repository.UserRepository;
 import org.apache.commons.io.FileUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,7 +14,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Base64;
 
 @RestController
 @RequestMapping("/api/img")
@@ -29,29 +27,24 @@ public class ImageController {
         this.ratingRepository = ratingRepository;
     }
 
-    User postUserImage(@RequestParam("image") MultipartFile file) throws IOException {
+    public void postUserImage(@RequestBody() MultipartFile file) throws IOException {
         String userId = SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
-        User user = userRepository.findUserById(userId);
-        File tempFile = File.createTempFile("Undecided-", ".tmp");
-        FileUtils.writeByteArrayToFile(tempFile, file.getBytes());
-        tempFile.deleteOnExit();
-        byte[] fileContent = FileUtils.readFileToByteArray(tempFile);
-        String encodedString = Base64.getEncoder().encodeToString(fileContent);
-        user.setProfileImage(encodedString);
-        return userRepository.save(user);
+        File imageFile = new File("/var/opt/pictures/users/Undecided-" + userId + ".jpg");
+        FileUtils.writeByteArrayToFile(imageFile, file.getBytes(), false);
     }
 
-    Rating postRatingImages(@RequestBody String ratingId, @RequestParam("image") MultipartFile file) throws IOException{
-        Rating rating = ratingRepository.findRatingById(ratingId);
-        File tempFile = File.createTempFile("Undecided-", ".tmp");
-        FileUtils.writeByteArrayToFile(tempFile, file.getBytes());
-        tempFile.deleteOnExit();
-        byte[] fileContent = FileUtils.readFileToByteArray(tempFile);
-        String encodedString = Base64.getEncoder().encodeToString(fileContent);
-        //rating.setImages(rating.getImages().add(encodedString););
-        return ratingRepository.save(rating);
+    public ResponseEntity postRatingImages(@RequestBody MultipartFile file, @RequestParam("rating") String ratingId, @RequestParam("index") String index) throws IOException{
+        int imageIndex = Integer.parseInt(index);
+        if (imageIndex > 4) {
+            return new ResponseEntity(HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+
+        File imageFile = new File("/var/opt/pictures/ratings/Undecided-" + ratingId + "-" + imageIndex + ".jpg");
+        if(!imageFile.exists()){
+
+        }
+        FileUtils.writeByteArrayToFile(imageFile, file.getBytes());
+
+        return new ResponseEntity(HttpStatus.CREATED);
     }
-
-
-
 }
