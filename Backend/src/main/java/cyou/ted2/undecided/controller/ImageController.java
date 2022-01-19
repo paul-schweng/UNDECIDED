eb215cd1ab277d65f6c3e390479281fafef8a242
Elisa Schäfer <elisa@elmama.de>
2022-01-19 20:16:42 +0100

    [UU-241] ImageController

--- Backend/src/main/java/cyou/ted2/undecided/controller/ImageController.java
+++ Backend/src/main/java/cyou/ted2/undecided/controller/ImageController.java
@@ -1,17 +1,13 @@
 package cyou.ted2.undecided.controller;
 
+import cyou.ted2.undecided.models.Rating;
 import cyou.ted2.undecided.repository.RatingRepository;
-import cyou.ted2.undecided.repository.UserRepository;
 import org.apache.commons.io.FileUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.File;
 import java.io.IOException;
 
@@ -19,32 +15,36 @@ import java.io.IOException;
 @RequestMapping("/api/img")
 public class ImageController {
 
-    private final UserRepository userRepository;
     private final RatingRepository ratingRepository;
+    private static final String PATH_TO_IMAGES = "/var/opt/pictures/";
 
-    public ImageController(UserRepository userRepository, RatingRepository ratingRepository) {
-        this.userRepository = userRepository;
+    public ImageController(RatingRepository ratingRepository) {
         this.ratingRepository = ratingRepository;
     }
 
-    public void postUserImage(@RequestBody() MultipartFile file) throws IOException {
+    @PostMapping("/user")
+    public ResponseEntity postUserImage(@RequestBody() MultipartFile file) throws IOException {
         String userId = SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
-        File imageFile = new File("/var/opt/pictures/users/Undecided-" + userId + ".jpg");
+        File imageFile = new File(PATH_TO_IMAGES + "users/" + userId + ".jpg");
         FileUtils.writeByteArrayToFile(imageFile, file.getBytes(), false);
+        return new ResponseEntity(HttpStatus.CREATED);
     }
 
+    @PostMapping("/rating")
     public ResponseEntity postRatingImages(@RequestBody MultipartFile file, @RequestParam("rating") String ratingId, @RequestParam("index") String index) throws IOException{
+        Rating rating = ratingRepository.findRatingById(ratingId);
         int imageIndex = Integer.parseInt(index);
         if (imageIndex > 4) {
             return new ResponseEntity(HttpStatus.UNPROCESSABLE_ENTITY);
         }
 
-        File imageFile = new File("/var/opt/pictures/ratings/Undecided-" + ratingId + "-" + imageIndex + ".jpg");
+        File imageFile = new File(PATH_TO_IMAGES + "ratings/" + ratingId + "-" + imageIndex + ".jpg");
         if(!imageFile.exists()){
-
+            rating.setImageNum(rating.getImageNum() + 1);
+            ratingRepository.save(rating);
         }
-        FileUtils.writeByteArrayToFile(imageFile, file.getBytes());
 
+        FileUtils.writeByteArrayToFile(imageFile, file.getBytes());
         return new ResponseEntity(HttpStatus.CREATED);
     }
 }
