0ec0af2876837c75413cdebd10a80d502e0604e6
Paul Schweng <paul.schweng@siemens.com>
2022-04-25 21:47:40 +0200

    [UU-328] Friends tab for popup on profile page

--- Backend/src/main/java/cyou/ted2/undecided/repository/FollowerRepository.java
+++ Backend/src/main/java/cyou/ted2/undecided/repository/FollowerRepository.java
@@ -17,4 +17,7 @@ public interface FollowerRepository extends JpaRepository<Following, FollowingPK
 
     @Query(value = "SELECT f.following FROM Following f WHERE f.user.id = ?1 AND f.followDate < ?2 ORDER BY f.followDate DESC")
     List<User> getAllUsingFollower_Id(String followingID, ZonedDateTime timestamp, Pageable pageable);
+
+    @Query(value = "SELECT f.user FROM Following f INNER JOIN User u ON u.id = f.user.id LEFT JOIN Following b ON b.user.id = f.following.id and b.following.id = u.id WHERE f.following.id = ?1 AND f.followDate < ?2 ORDER BY f.followDate DESC", nativeQuery = false)
+    List<User> getAllFriends(String userId, ZonedDateTime timestamp, Pageable pageable);
 }
